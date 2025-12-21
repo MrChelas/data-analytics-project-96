c041590de210e1480036b2cf2f199e2f78a78217
Sergei <dushku88@gmail.com>
2025-12-21 16:01:35 +0300

    v2

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,24 +1,22 @@
 
-with
-    last_visit as (
-    select 
-        *,
-        row_number() over(partition by s.visitor_id order by s.visit_date desc) as rn
-    from sessions s
-    )
+with 
+last_visit as (
+	select *,
+		row_number() over(partition by s.visitor_id order by s.visit_date desc) as rn
+	from sessions s
+)
 
 select 
-    la.visitor_id,
-    la.visit_date, 
-    la.medium,  
+    lv.visitor_id,
+    lv.visit_date, 
+    lv.medium,  
     l.lead_id,
     l.created_at,
     l.amount,
     l.closing_reason,
-    l.status_id, rn
-from last_visit la
+    l.status_id
+from last_visit lv
 left join leads l
-    on la.visitor_id = l.visitor_id
-where la.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') and rn = 1
-order by amount desc nulls last, visit_date, medium
-limit 10;
+	on lv.visitor_id = l.visitor_id and lv.visit_date < l.created_at
+where lv.medium <> 'organic' and rn = 1
+order by amount desc nulls last, visit_date, medium;
