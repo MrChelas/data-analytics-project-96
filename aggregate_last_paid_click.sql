2ff3d2b72b9b9dc11e8b9b8eff9c4fbb4bedbe1c
Sergei <dushku88@gmail.com>
2026-03-27 08:44:26 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,55 +1,75 @@
-with 
-vk1 as (
+with ads_data as (
 	select
-		date_trunc('day', campaign_date) as date_vk,
+		date(campaign_date) as visit_date,
 		utm_source,
 		utm_medium,
 		utm_campaign,
-		sum(daily_spent) as vk_spent
+		sum(daily_spent) as total_spent
 	from vk_ads
-	group by date_vk, utm_source, utm_medium, utm_campaign
-),
+	group by date(campaign_date), utm_source, utm_medium, utm_campaign
 
-ya1 as (
+UNION ALL
+
 	select
-		date_trunc('day', campaign_date) as date_ya,
+		date(campaign_date) as visit_date,
 		utm_source,
 		utm_medium,
 		utm_campaign,
-		sum(daily_spent) as ya_spent
+		sum(daily_spent) as total_spent
 	from ya_ads
-	group by date_ya, utm_source, utm_medium, utm_campaign
+	group by date(campaign_date), utm_source, utm_medium, utm_campaign
 ),
 
-purchases as (
-	select
-		date_trunc('day', s.visit_date) as visit_date,
-		source, medium, campaign,
-		count(s.visitor_id) as visitors_count,
-		count(created_at) as leads_count,
-		count(created_at) filter(where status_id = 142) as purchases_count,
-		sum(coalesce(l.amount, 0)) as revenue
-	from (
-			select *, row_number() over(partition by visitor_id order by visit_date desc) as rn
-	  		from sessions) s
-	left join leads l
-		on s.visitor_id = l.visitor_id
-	where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social') and rn = 1
-	group by date_trunc('day', s.visit_date), source, medium, campaign
+last_paid_click as (
+select
+    s.visitor_id,
+    date(s.visit_date) as visit_date, 
+    s.medium,
+    s.campaign,
+    s.source,  
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id,
+    row_number() over(partition by s.visitor_id order by s.visit_date desc) as rn
+from sessions as s
+left join leads l
+    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+
+agg_data as (
+    select
+	source as utm_source,
+	medium as utm_medium,
+	campaign as utm_campaign,
+	visit_date,
+	count(visitor_id) as visitors_count,
+	count(created_at) filter(where created_at is not null) as leads_count,
+	count(created_at) filter(where status_id = 142) as purchases_count, --visitor_id--
+	sum(amount) filter(where status_id = 142) as revenue
+    from last_paid_click
+    where rn = 1
+    group by utm_source, utm_medium, utm_campaign, visit_date
 )
 
-select 
-	visit_date,
-	visitors_count,
-	source, 
-	medium,
-	campaign,
-	coalesce(vk_spent, 0) + coalesce(ya_spent, 0) as total_cost,
-	leads_count, purchases_count, revenue
-from purchases p
-left join vk1
-	on p.visit_date = vk1.date_vk and p.source = vk1.utm_source and p.campaign = vk1.utm_campaign
-left join ya1
-	on p.visit_date = ya1.date_ya and p.source = ya1.utm_source and p.campaign = ya1.utm_campaign
-order by visit_date, visitors_count desc, source, medium, campaign, revenue desc nulls last
-;
+select
+    a.visit_date,
+    a.visitors_count,
+    a.utm_source,
+    a.utm_medium,
+    a.utm_campaign,
+    coalesce(m.total_spent, 0) as total_cost,
+    a.leads_count,
+    a.purchases_count, 
+    a.revenue
+from agg_data as a
+left join ads_data as m
+on a.visit_date = m.visit_date and
+lower(a.utm_source) = lower(m.utm_source) and
+lower(a.utm_medium) = lower(m.utm_medium) and 
+lower(a.utm_campaign) = lower(m.utm_campaign)
+order by 
+    revenue desc nulls last, visit_date, visitors_count desc, utm_source, utm_medium, utm_campaign
+limit 15;
--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,22 +1,35 @@
-
 with 
 last_visit as (
-	select *,
-		row_number() over(partition by s.visitor_id order by s.visit_date desc) as rn
-	from sessions s
-)
-
-select 
-    lv.visitor_id,
-    lv.visit_date, 
-    lv.medium,  
+select
+    s.visitor_id,
+    s.visit_date, 
+    s.medium as UTM_medium,
+    s.campaign as UTM_campaign,
+    s.source as UTM_source,  
     l.lead_id,
     l.created_at,
     l.amount,
     l.closing_reason,
-    l.status_id
-from last_visit lv
+    l.status_id,
+    row_number() over(partition by s.visitor_id order by s.visit_date desc) as rn
+from sessions as s
 left join leads l
-	on lv.visitor_id = l.visitor_id and lv.visit_date < l.created_at
-where lv.medium <> 'organic' and rn = 1
-order by amount desc nulls last, visit_date, medium;
+    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+)
+
+select 
+    visitor_id,
+    date(visit_date),
+    UTM_source,
+    UTM_medium,
+    UTM_campaign,  
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from last_visit
+where rn = 1
+order by amount desc nulls last, visit_date, UTM_source, UTM_medium, UTM_campaign
+limit 10;
